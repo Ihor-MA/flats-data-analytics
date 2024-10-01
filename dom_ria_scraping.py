daad7be9f8e2ab6f1c116e137be9c8f23ed9c489
Igor Berezivskyi <igor200305.ber@gmail.com>
2024-10-01 21:35:30 +0300

    Deleted extra print

--- dom_ria_scraping.py
+++ dom_ria_scraping.py
@@ -245,7 +245,6 @@ async def main():
     if start_page == 1:
         write_titles_for_flats_csv()
     await write_all_flats_to_csv(start_page, end_page)
-    print("Data is written!")
 
 
 if __name__ == "__main__":
