776e6198f45cea35bcad8006c83133c347f355bb
Igor Berezivskyi <igor200305.ber@gmail.com>
2024-10-01 21:01:39 +0300

    Added a main function that contains 2 functions for writing data to a csv file

--- dom_ria_scraping.py
+++ dom_ria_scraping.py
@@ -1,4 +1,14 @@
 import asyncio
 
+
+async def main():
+    start_page = 1
+    end_page = 3
+    if start_page == 1:
+        write_titles_for_flats_csv()
+    await write_all_flats_to_csv(start_page, end_page)
+    print("Data is written!")
+
+
 if __name__ == "__main__":
     asyncio.run(main())
